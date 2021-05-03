c839a1c128dd432017d58b2fe624334e88071d37
BorMooh <bor.mohoric2@gmail.com>
2021-05-03 21:42:17 +0200

    Commit

---
+++ PaintTool/Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PaintTool
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        Graphics g;
+        bool startPaint = false;
+        int? initX = null;
+        int? initY = null;
+
+        //Za izbris canvasa
+        bool canBool = false;
+
+        //Booli za preverjanje kater lik je uporabnikh izbral
+        bool elipse = false;
+        bool rectangle = false;
+
+        //Pen p je namenjem črtam, Pen pliki pa likom
+        //Vsak pen je posebej ker imamo hkrati lahko drugači width za like in črte
+        Pen p = new Pen(Color.Black, 5);
+        Pen pliki = new Pen(Color.Black, 5);
+
+        int brushSize = 5;
+        Bitmap bmp;
+
+        //Test
+        public static string lik = "";
+
+        //Ali je bila slika imported?
+        bool imported = false;
+        string pathTemp = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Novi bitmap - podloga za risanje(papir)
+            bmp = new Bitmap(panel1.ClientSize.Width, panel1.ClientSize.Height);
+
+            //Spreminjanje začetka in konca črte - da sta zaobljena
+            p.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            p.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            pliki.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            pliki.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+
+        }
+
+        #region Panel1
+        //Ustvarjanje likov
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            startPaint = true;
+
+            //Za pridobivanje centra lika.
+            int diff =  Form2.F2Elwidth / 2;
+            //Brush size iz Form2
+            pliki.Width = Form2.brushSize;
+
+            //Za risanje likov
+            using (g = Graphics.FromImage(bmp))
+            {
+                //Če je katerkoli od njiju true se izvede spodji del kode
+                if (elipse || rectangle)
+                {
+                    if (elipse) //Ali je uporabnik izbral elipso?
+                    {
+                        g.DrawEllipse(pliki, e.X - diff, e.Y - diff, Form2.F2Elwidth, Form2.F2Elheigth);
+                    }
+                    else if (rectangle) //Ali pravotoknik?
+                        g.DrawRectangle(pliki, e.X - diff, e.Y - diff, Form2.F2Rewidth, Form2.F2Reheigth);
+
+
+                    //Na koncu oba booleana nastavimo na false
+                    elipse = false;
+                    rectangle = false;
+                }
+            }
+            panel1.Refresh();
+            g.Dispose();
+
+        }
+
+        //Ustvarjanje črt z miško
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (startPaint)
+            {
+                using (g = Graphics.FromImage(bmp))
+                {
+                    //Ob kliku na "Clear canvas"
+                    if (canBool)
+                    {
+                        g.Clear(panel1.BackColor);
+                        canBool = false;
+                    }
+
+                    //Da so črte lepše
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+                    //Risanje dejanske črte
+                    g.DrawLine(p, new Point(initX ?? e.X, initY ?? e.Y), new Point(e.X, e.Y));
+                    initX = e.X;
+                    initY = e.Y;
+                }
+                panel1.Refresh();
+                g.Dispose();
+            }
+        }
+
+        //Prenehanje z risanjem
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+                startPaint = false;
+                initX = null;
+                initY = null;
+
+        }
+
+        #endregion Panel1
+
+        //Color picker dialog
+        private void colorPickerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if(cd.ShowDialog() == DialogResult.OK)
+            {
+                //Definiranje barve za PEN
+                p.Color = cd.Color;
+                pliki.Color = cd.Color;
+
+                //Definiranje foregrond in background barve za TEKST v meniju
+                toolStripColor.BackColor = cd.Color;
+            }
+
+        }
+
+        //Save 
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+            //Ali je bila slika imported?
+            if(imported && pathTemp.Length != 0)
+            {
+                if(File.Exists(pathTemp))
+                {
+                    //Vsa koda je namenjena da uporabnik vidi da se shranjuje
+                    Cursor.Current = Cursors.WaitCursor;
+                    Application.DoEvents();
+                    Thread.Sleep(700);
+
+                    //Datoteko izbrišemo in shranimo z istim imenom
+                    File.Delete(pathTemp);
+                    bmp.Save(pathTemp);
+
+                    //Naredimo Thread za prikaz teksta in cursor nastavimo na privzetega
+                    Thread tekst = new Thread(StatusBarSaveTekst);
+                    tekst.Start();
+                    Cursor.Current = Cursors.Default;
+                }
+
+                else
+                {
+                    MessageBox.Show("datoteka ne obstaja!");
+                }
+            }
+            //Če datoteke nismo importali nas vodi direktno do metode Save-As
+            else
+            {
+                saveAsToolStripMenuItem_Click(sender, e);
+            }
+        }
+
+
+        //Save-As file dialog
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Image | *.jpeg";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+
+                if(System.IO.File.Exists(sfd.FileName))
+                {
+                    System.IO.File.Delete(sfd.FileName);
+                }
+
+                bmp.Save(sfd.FileName);
+                
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(bmp, Point.Empty);
+
+        }
+
+        //Če se form resize-a, se na novo naredi Bitmap
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(panel1.ClientSize.Width, panel1.ClientSize.Height);
+        }
+
+        //Tipke za spreminjanje debeline svincnika
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Add)
+            {
+                brushSize += 1;
+                p.Width = brushSize;
+            }
+            else if(e.KeyCode == Keys.Subtract)
+            {
+                if (brushSize > 0)
+                {
+                    brushSize -= 1;
+                    p.Width = brushSize;
+                }
+            }
+
+
+            toolStripPenWidth.Text = "Pen width: " + brushSize;
+        }
+
+        //Ob kliku na clear canvas
+        private void clearCanvasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            canBool = true;
+
+            //Za SAVE
+            imported = false;
+            pathTemp = "";
+        }
+
+        //Meni za odpiranje slike in nastavljanje višine in širine
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opd = new OpenFileDialog();
+            opd.Filter = "Images | *.jpg;*.jpeg;*.png;*.ico |" +
+                         "All files | *.*";
+
+            if(opd.ShowDialog() == DialogResult.OK)
+            {
+                //Image se import-a v Visual Studio preko FileStreama kar je potrebno da se sliko lahko shrani v isti
+                //ime kot je bila prej = "Shrani namesto Shrani-kot"
+                Image curImg;
+                using (FileStream fs = new FileStream(opd.FileName, FileMode.Open))
+                {
+                    curImg = Image.FromStream(fs);
+                    fs.Close();
+                }
+
+                //Izračunavanje širino glede na višino slike
+                //ČE je slika višja kot širša je formula drugačna 
+                int calc;
+                if (curImg.Height > curImg.Width)
+                    calc = (curImg.Height / curImg.Width) * panel1.Height;
+
+                //Če je slika širša kot visoka
+                else
+                    calc = (curImg.Width / curImg.Height) * panel1.Width;
+
+                bmp = new Bitmap(curImg, calc, panel1.Height);
+
+
+                imported = true;
+                pathTemp = opd.FileName;
+            }
+
+        }
+
+        //Program nas vpraša če res zelimo zapreti program
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult clos = MessageBox.Show("Do you want to close Paint?", "Paint", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(clos != DialogResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        //Zamenjava barve ozadja - ob zamenjavi ozadja se vse stvari na canvasu izbrišejo!
+        private void backgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            
+            if(cd.ShowDialog() == DialogResult.OK)
+            {
+                panel1.BackColor = cd.Color;
+
+                //Ko se zamenja barva panela se bool "canBool" nastavi na true - kar potem v
+                //panel1_mousemove metodi sproži Clear in počisti zaslon in spremeni barvo
+                canBool = true;
+            }
+        }
+
+        //Info menu item
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Odpre nov form/mbox in napiše informacije o projektu.
+            string stavek = "Avtor: Bor Mohorič\n" +
+                            "29.4.2021\n" +
+                            "To je testni projekt.";
+            MessageBox.Show(stavek, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        //Preverjanje kateri gradnik iz DRAW menija je bil izbran
+        private void elipseToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (sender.ToString() == "Elipse")
+                elipse = true;
+
+            else if (sender.ToString() == "Rectangle")
+                rectangle = true;
+
+        }
+
+        //"Properties" okno za vsak lik
+        private void setShapeProperties(object sender, EventArgs e)
+        {
+            //Pridobivanje imena od parenta iz MenuStripToolItem
+            lik = Convert.ToString(((ToolStripMenuItem)sender).OwnerItem);
+
+
+            Form2 setting = new Form2();
+            setting.Show();
+
+
+        }
+
+        //--------------------------------------------------------------------------------------------------------------//
+        //----------------------------------------------------METODE----------------------------------------------------//
+        //--------------------------------------------------------------------------------------------------------------//
+        #region Metode
+        //Metoda ki nam določa barvo foregrounda glede na barvo backgrounda
+        Color Kontrast(Color color)
+        {
+            int d = 0;
+            double luminance = (0.299 * color.R + 0.587 * color.G + 0.114 * color.B) / 255;
+            if (luminance > 0.5)
+                d = 0; 
+            else
+                d = 255; 
+            return Color.FromArgb(d, d, d);
+        }
+
+        //Metoda ki nam spodaj v oknu napiše da je bila datoteka shranjena. 
+        private void StatusBarSaveTekst()
+        {
+
+            toolStripStatusSaved.Text = "File has been saved!";
+            toolStripStatusSaved.BackColor = Color.Yellow;
+            Thread.Sleep(5000);
+            toolStripStatusSaved.Text = "";
+
+            Thread.CurrentThread.Abort();
+        }
+
+        #endregion Metode
+
+    }
+}
---
+++ PaintTool/Form2.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace PaintTool
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbWid = new System.Windows.Forms.TextBox();
+            this.tbHei = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBarW = new System.Windows.Forms.TrackBar();
+            this.trackBarH = new System.Windows.Forms.TrackBar();
+            this.tbBru = new System.Windows.Forms.NumericUpDown();
+            this.btnLock = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarW)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBru)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(613, 590);
+            this.panel1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(613, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.closeToolStripMenuItem.Text = "Close";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(623, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ITEM_NAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(619, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "WIDTH:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(619, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "HEIGHT:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(626, 331);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "BRUSH:";
+            // 
+            // tbWid
+            // 
+            this.tbWid.Location = new System.Drawing.Point(704, 147);
+            this.tbWid.Name = "tbWid";
+            this.tbWid.Size = new System.Drawing.Size(100, 20);
+            this.tbWid.TabIndex = 5;
+            this.tbWid.Leave += new System.EventHandler(this.tbWid_Leave);
+            // 
+            // tbHei
+            // 
+            this.tbHei.Location = new System.Drawing.Point(704, 242);
+            this.tbHei.Name = "tbHei";
+            this.tbHei.Size = new System.Drawing.Size(100, 20);
+            this.tbHei.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(641, 515);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 54);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "GENERATE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBarW
+            // 
+            this.trackBarW.Location = new System.Drawing.Point(622, 173);
+            this.trackBarW.Maximum = 1000;
+            this.trackBarW.Name = "trackBarW";
+            this.trackBarW.Size = new System.Drawing.Size(182, 45);
+            this.trackBarW.TabIndex = 9;
+            this.trackBarW.Scroll += new System.EventHandler(this.scroll);
+            // 
+            // trackBarH
+            // 
+            this.trackBarH.Location = new System.Drawing.Point(622, 268);
+            this.trackBarH.Maximum = 1000;
+            this.trackBarH.Name = "trackBarH";
+            this.trackBarH.Size = new System.Drawing.Size(182, 45);
+            this.trackBarH.TabIndex = 10;
+            this.trackBarH.Scroll += new System.EventHandler(this.scroll);
+            // 
+            // tbBru
+            // 
+            this.tbBru.Location = new System.Drawing.Point(704, 331);
+            this.tbBru.Name = "tbBru";
+            this.tbBru.Size = new System.Drawing.Size(75, 20);
+            this.tbBru.TabIndex = 11;
+            this.tbBru.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // btnLock
+            // 
+            this.btnLock.Location = new System.Drawing.Point(677, 208);
+            this.btnLock.Name = "btnLock";
+            this.btnLock.Size = new System.Drawing.Size(75, 28);
+            this.btnLock.TabIndex = 12;
+            this.btnLock.Text = "LOCK";
+            this.btnLock.UseVisualStyleBackColor = true;
+            this.btnLock.Click += new System.EventHandler(this.btnLock_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(831, 590);
+            this.Controls.Add(this.btnLock);
+            this.Controls.Add(this.tbBru);
+            this.Controls.Add(this.trackBarH);
+            this.Controls.Add(this.trackBarW);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbHei);
+            this.Controls.Add(this.tbWid);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form2";
+            this.Text = "Properties";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarW)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBru)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbWid;
+        private System.Windows.Forms.TextBox tbHei;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBarW;
+        private System.Windows.Forms.TrackBar trackBarH;
+        private System.Windows.Forms.NumericUpDown tbBru;
+        private System.Windows.Forms.Button btnLock;
+    }
+}
